47f35d81b92bec73a1579f0b4d06e3260bd5e2e7
Naveen-kumaar <navinsanjay2001@gmail.com>
2024-12-13 09:22:17 +0530

    adding email js script

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+document.getElementById('contact-form').addEventListener('submit',(e)=>{
+    e.preventDefault()
+    let params = {
+        form_name: document.getElementById('full_name').value,
+        email_id: document.getElementById('email_id').value,
+        message: document.getElementById('message').value,
+    }
+    
+    emailjs.send("service_loxitnt","template_mm4640b",params).then(()=>{
+        mss=document.querySelector('.thank-you-msg').computedStyleMap.display ="block"
+        document.getElementById('full_name').value="",
+        document.getElementById('email_id').value="",
+        document.getElementById('message').value="",
+    
+        setTimeout(()=>{
+            time=document.querySelector('.thank-you-msg').style.display="none"
+            console.log(time)
+        },2500)
+        
+    })
+})
